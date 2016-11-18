981e4b5e6796bbfac290aadf56df08eaf7900721
Yangqin Cao <caoyangqin0607@163.com>
2016-11-18 14:08:18 +0800

    init the gallery stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,29 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+
+/*获取图片数据*/
+var imageDatas=require('../data/imageData.json');
+/*获取图片路径信息*/
+imageDatas=(function genImageURL(imageDataArr){
+	for(var i=0;i<imageDataArr.length;i++){
+		var singleImageData=imageDataArr[i];
+		singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+		imageDataArr[i]=singleImageData;
+	}
+	return imageDataArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+      		<section className="img-sec">
+      		</section>
+			<nav className="controller-nav">
+			</nav>
+      </section>
     );
   }
 }
